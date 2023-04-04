23dea4486e5a5f158bb33adb9f8ab5b82c4f851a
Vitali Bedik <vitalya.bedik@gmail.com>
2023-04-04 21:50:35 +0300

    feat: finish 03 lesson part2 - universal buttons

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,29 @@ import React from 'react';
 
 import './App.css';
 
-import {MyButton} from './components/03-MyButton/MyButton';
+import {Button} from './components/03-MyButton/Button';
 
 function App() {
+    const Button1Foo = (subscriber: string, age: number, address: string) => {
+        console.log(subscriber, age, address)
+    }
+
+    const Button2Foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
+    }
+
+    const doSmthButton = () => {
+        console.log('i cant do smth, im stupid button')
+    }
 
     return (
         <>
-            <MyButton/>
+            {/*<button>MyYouTubeChannel-1</button>*/}
+            {/*<button>MyYouTubeChannel-2</button>*/}
+
+            <Button name={'MyYouTubeChannel-1'} callback={() => Button1Foo('im Vasya!', 21, 'live in Minsk')}/>
+            <Button name={'MyYouTubeChannel-2'} callback={() => Button2Foo('im Ivan!', 30)}/>
+            <Button name={'Im stupid button'} callback={doSmthButton}/>
         </>
 
     );
---
+++ src/components/03-MyButton/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type ButtonPropsType = {
+    name: string
+    callback: () => void
+
+}
+
+export const Button: React.FC<ButtonPropsType> = (props) => {
+
+    const onClickHandler = () => {
+        props.callback()
+    }
+    return (
+        <button onClick={onClickHandler}>{props.name}</button>
+    )
+}
