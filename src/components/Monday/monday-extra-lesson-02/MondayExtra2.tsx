aeafa219531d2e692f6fe141e0b2f3c260b7091d
Vitali Bedik <vitalya.bedik@gmail.com>
2023-04-14 21:41:05 +0300

    feat: finish extra task monday-extra-lesson-02 (ref)

--- src/components/Monday/monday-extra-lesson-02/MondayExtra2.tsx
+++ src/components/Monday/monday-extra-lesson-02/MondayExtra2.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 import {SuperButton} from './components/SuperButton';
 import {SuperInput} from './components/SuperInput';
@@ -14,10 +14,7 @@ export function MondayExtra2() {
     // неправильная структура проекта, нужно
     const [todos, setTodos] = useState<TodosType[]>([])
 
-    const [title, setTitle] = useState('')
-
-    // поменять useState на useRef
-    // const [title, setTitle] = useRef('')
+    let myRef = useRef<HTMLInputElement>(null)
 
     const fetchRequest = () => {
         fetch('https://jsonplaceholder.typicode.com/todos')
@@ -38,30 +35,25 @@ export function MondayExtra2() {
     }
 
     const addTaskHandler = () => {
-        // возьми title и отправь setTodos(title)
-        const newTodo: TodosType = {
-            userId: 5,
-            id: todos.length + 1,
-            title: title,
-            completed: false
-        }
-        setTodos([newTodo, ...todos])
-        setTitle('')
+           if (myRef.current) {
+               const newTodo: TodosType = {
+                   userId: 5,
+                   id: todos.length + 1,
+                   title:'123',
+                   completed: false
+               }
+               setTodos([newTodo, ...todos])
+               myRef.current.value = ''
+           }
     }
 
     return (
         <div className="App">
             <div>
-                {/*<input type="text"/>*/}
-                {/*<button>+</button>*/}
-
-                <SuperInput title={title} setTitle={setTitle}  />
+                <SuperInput title={myRef} />
                 <SuperButton name={'+'} callback={addTaskHandler}/>
             </div>
             <div>
-                {/*<button onClick={showTodosHandler}>Show me todos</button>*/}
-                {/*<button onClick={hideTodosHandler}>Hide todos</button>*/}
-
                 <SuperButton name={'Show me todos'} callback={showTodosHandler}/>
                 <SuperButton name={'Hide todos'} callback={hideTodosHandler}/>
             </div>
@@ -80,4 +72,92 @@ export function MondayExtra2() {
         </div>
     );
 }
+
+
+//===========================================================
+
+
+// import React, {useEffect, useState} from 'react';
+//
+// import {SuperButton} from './components/SuperButton';
+// import {SuperInput} from './components/SuperInput';
+//
+// type TodosType = {
+//     userId: number
+//     id: number
+//     title: string
+//     completed: boolean
+// }
+//
+// export function MondayExtra2() {
+//     // неправильная структура проекта, нужно
+//     const [todos, setTodos] = useState<TodosType[]>([])
+//
+//     const [title, setTitle] = useState('')
+//
+//     // поменять useState на useRef
+//     // const [title, setTitle] = useRef('')
+//
+//     const fetchRequest = () => {
+//         fetch('https://jsonplaceholder.typicode.com/todos')
+//             .then((response) => response.json())
+//             .then((json) => setTodos(json));
+//     }
+//
+//     useEffect(() => {
+//         fetchRequest()
+//     }, [])
+//
+//     const showTodosHandler = () => {
+//         fetchRequest()
+//     }
+//
+//     const hideTodosHandler = () => {
+//         setTodos([])
+//     }
+//
+//     const addTaskHandler = () => {
+//         // возьми title и отправь setTodos(title)
+//         const newTodo: TodosType = {
+//             userId: 5,
+//             id: todos.length + 1,
+//             title: title,
+//             completed: false
+//         }
+//         setTodos([newTodo, ...todos])
+//         setTitle('')
+//     }
+//
+//     return (
+//         <div className="App">
+//             <div>
+//                 {/*<input type="text"/>*/}
+//                 {/*<button>+</button>*/}
+//
+//                 <SuperInput title={title} setTitle={setTitle}  />
+//                 <SuperButton name={'+'} callback={addTaskHandler}/>
+//             </div>
+//             <div>
+//                 {/*<button onClick={showTodosHandler}>Show me todos</button>*/}
+//                 {/*<button onClick={hideTodosHandler}>Hide todos</button>*/}
+//
+//                 <SuperButton name={'Show me todos'} callback={showTodosHandler}/>
+//                 <SuperButton name={'Hide todos'} callback={hideTodosHandler}/>
+//             </div>
+//             <ul>
+//                 {todos.map(todo => {
+//                     return (
+//                         <li key={todo.id}>
+//                             <span>{todo.id}</span>
+//                             <span> _ {todo.userId}</span>
+//                             <span> _ {todo.title}</span>
+//                             <input type="checkbox" checked={todo.completed}/>
+//                         </li>
+//                     )
+//                 })}
+//             </ul>
+//         </div>
+//     );
+// }
+
 
--- src/components/Monday/monday-extra-lesson-02/components/SuperInput.tsx
+++ src/components/Monday/monday-extra-lesson-02/components/SuperInput.tsx
@@ -1,19 +1,43 @@
-import {ChangeEvent, useState} from 'react';
+import {ChangeEvent, LegacyRef} from 'react';
 
 type PropsType = {
-    title: string
-    setTitle: (title: string) => void
+    title: LegacyRef<HTMLInputElement> | undefined
 }
-
 
 export const SuperInput = (props: PropsType) => {
-    // const [title, setTitle] = useState('')
-
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        props.setTitle(e.currentTarget.value)
-    }
+        const onChangeHandler = () => props.title
 
     return (
-            <input value={props.title} onChange={onChangeHandler}/>
+            <input ref={props.title} onChange={onChangeHandler} />
         )
 }
+
+
+
+
+
+
+
+
+
+//========================================================================================
+
+// import {ChangeEvent, useState} from 'react';
+//
+// type PropsType = {
+//     title: string
+//     setTitle: (title: string) => void
+// }
+//
+//
+// export const SuperInput = (props: PropsType) => {
+//     // const [title, setTitle] = useState('')
+//
+//     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+//         props.setTitle(e.currentTarget.value)
+//     }
+//
+//     return (
+//         <input  value={props.title} onChange={onChangeHandler}/>
+//     )
+// }
