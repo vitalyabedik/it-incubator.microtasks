651fda1a3b9395c7a587cd4da95441377c3746ac
Vitali Bedik <vitalya.bedik@gmail.com>
2023-04-23 09:52:30 +0300

    feat: start template 07-AssociativeArray

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './App.css';
 
-import {MondayExtra2Hw} from './components/Monday/monday-hw-help-02/MondayExtra2Hw';
+import {AssociativeArray} from './components/Monday/07-associative-array/AssociativeArray';
 
 function App() {
 
     return (
         <>
-            <MondayExtra2Hw/>
+            <AssociativeArray/>
         </>
 
     );
--- src/components/Monday/monday-extra-lesson-01-homework/MondayExtra1Hw.tsx
+++ src/components/Monday/monday-extra-lesson-01-homework/MondayExtra1Hw.tsx
@@ -83,7 +83,7 @@ export const MondayExtra1Hw = () => {
 //----------------------------------------------------------------------------------------
 
 // import React, {useEffect, useState} from 'react';
-// import './App.css';
+// import './AssociativeArray.css';
 // import axios from "axios";
 //
 //
