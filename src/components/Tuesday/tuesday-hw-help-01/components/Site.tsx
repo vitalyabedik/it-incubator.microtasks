439a71613803adecd733dc31ae094049fd114e6b
Vitali Bedik <vitalya.bedik@gmail.com>
2023-04-28 20:49:39 +0300

    feat: tuesday-hw-help-01 basic logic

--- src/components/Tuesday/tuesday-hw-help-01/components/Site.tsx
+++ src/components/Tuesday/tuesday-hw-help-01/components/Site.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
+
+
 import styles from './Site.module.css'
+import {PageOne} from './pages/PageOne';
+import {PageTwo} from './pages/PageTwo';
+import {PageThree} from './pages/PageThree';
+import {Error404} from './pages/Error404';
 
 
 export const Site = () => {
@@ -8,10 +15,24 @@ export const Site = () => {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    1234
+                    <div><NavLink className={({isActive}) => isActive ? styles.active : styles.navLink} to={'/page1'}>Page 1</NavLink></div>
+                    <div><NavLink className={({isActive}) => isActive ? styles.active : styles.navLink} to={'/page2'}>Page 2</NavLink></div>
+                    <div><NavLink className={({isActive}) => isActive ? styles.active : styles.navLink} to={'/page3'}>Page 3</NavLink></div>
+
+                    <p><a href='/page3'>Нативная ссылка</a></p>
                 </div>
                 <div className={styles.content}>
-               content
+
+                    <Routes>
+                        <Route path={'/'} element={<Navigate to={'/page1'}/>}/>
+
+                        <Route path={'/page1'} element={<PageOne/>}/>
+                        <Route path={'/page2'} element={<PageTwo/>}/>
+                        <Route path={'/page3'} element={<PageThree/>}/>
+
+                        <Route path={'*'} element={<Error404/>}/>
+                    </Routes>
+
                 </div>
             </div>
         </div>
---
+++ src/components/Tuesday/tuesday-hw-help-01/components/pages/Page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Page = () => {
+    return (
+        <div>
+            PAGE MAIN
+        </div>
+    );
+};
+
