ef4c3644858f9406a35036f3da579f738ed10272
Vitali Bedik <vitalya.bedik@gmail.com>
2023-05-04 17:53:30 +0300

    feat: tuesday-hw-help-01 condition for existing pages

--- src/components/Tuesday/tuesday-hw-help-01/components/pages/Page.tsx
+++ src/components/Tuesday/tuesday-hw-help-01/components/pages/Page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {dataState, PagesType} from '../dataState/dataState';
 import {useParams} from 'react-router-dom';
+import {Error404} from './Error404';
 
 type PropsType = {
     pages: Array<PagesType>
@@ -8,14 +9,20 @@ type PropsType = {
 
 export const Page = (props: PropsType) => {
     const params = useParams()
+    const page = props.pages[Number(params.id)]
     console.log(typeof params.id) // string
     console.log(props.pages[Number(params.id)])
 
     return (
         <div>
-            // если
-            <div>{props.pages[Number(params.id)].heading}</div>
-            <div>{props.pages[Number(params.id)].about}</div>
+            {page ?
+                <div>
+                    <div>{props.pages[Number(params.id)].heading}</div>
+                    <div>{props.pages[Number(params.id)].about}</div>
+                </div>
+                :
+                <Error404/>
+            }
         </div>
     );
 };
